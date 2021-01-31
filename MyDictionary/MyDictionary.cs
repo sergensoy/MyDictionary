fb093b9eb3e7baf74afac9d2ad9f4feb61ddbba6
Sergen <Serge@DESKTOP-03I71OO>
2021-01-31 17:25:15 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key,V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return keys.Length; int Length = values.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Sergen");
+            myDictionary.Add(2, "Ahmet");
+            myDictionary.Add(3, "Ali");
+            myDictionary.Add(4, "Veli");
+            myDictionary.Add(5, "Mesut");
+            myDictionary.Add(6, "Pelgas");
+            myDictionary.Add(7, "İrfan");
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
